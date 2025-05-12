d85adbb56c9011a05eba0f72e66c4206486a119b
gpatlassian <111025158+gpatlassian@users.noreply.github.com>
2025-05-12 15:43:07 -0300

    Update securitybreach.js GT-2

--- securitybreach.js
+++ securitybreach.js
@@ -21,7 +21,7 @@ const person = {
 console.log("Name: " + person.name);
 console.log("Age: " + person.age);
 console.log("City: " + person.city);
-console.log("City: " + "password: password123");
+console.log("City: " + "password: ATATT3xFfGF0SBGZoycZxqHXTWsQbr7Ms3341Q8hFZ1nUF4i-OO4sB83bTepSvMnw0K9q5QafhDM0YXgJXbLzBMfM1F9vjD-2o6XCE3nuXQLDjxzGVOSBqtBzPvDXdFKH5L5RSN8sEYZCKp_3epggaMRaWo8x47nPXppM4SvKym7WQPN35stmVA=DC4FDA7E");
 
 // Array example
 const numbers = [1, 2, 3, 4, 5];
